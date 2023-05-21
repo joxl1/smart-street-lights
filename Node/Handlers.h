b21413078dabb792e779e8ea25d07f2377b50b82
joxl1 <128135281+joxl1@users.noreply.github.com>
2023-05-21 00:16:57 -0700

    Update Handlers.h

--- Node/Handlers.h
+++ Node/Handlers.h
@@ -1,4 +1,19 @@
-void HandleData() {}
-void HandlePairing() {}
-void HandleCommand() {}
+void HandleData() {
+}
+void HandlePairing() {
+  switch (pairingStatus) {
+    case REQUEST_PAIRING:
+      RequestPairing();
+      break;
+    case PAIRING_INITIATED:
+      CheckForTimeout();
+      break;
+    case PAIRING_COMPLETE:
+      // nothing to do here
+      break;
+  }
+  
+}
+void HandleCommand() {
+}
 
